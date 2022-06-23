a65014c026bde0b15f94b49c440466b4210e1712
Ricardo Paes <ricardo@likesistemas.com.br>
2022-06-23 10:09:39 -0300

    💚 Fixing bugs using PHP 5.6

--- db/migrations/20220623110507_my_new_migration.php
+++ db/migrations/20220623110507_my_new_migration.php
@@ -1,10 +1,9 @@
 <?php
-declare(strict_types=1);
 
 use Phinx\Migration\AbstractMigration;
 
 final class MyNewMigration extends AbstractMigration {
-	public function change(): void {
+	public function change() {
 		$table = $this->table('user_logins');
 		$table->addColumn('user_id', 'integer')
 			  ->addColumn('created', 'datetime')
